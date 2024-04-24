cc27e152e610559d59b41b4efa642a69bf7b8ef9
zaynpanjwani <zaynpanjwani@gmail.com>
2024-04-24 16:22:11 +0100

    Fixed the simulator not rendering until it is resized.

--- Part 2/RaceUI.java
+++ Part 2/RaceUI.java
@@ -4,6 +4,8 @@ import java.awt.*;
 public class RaceUI extends JFrame {
 
     private HorseLanes horseLanes;
+    private JPanel horseInfo;
+    private Settings customization;
     public RaceUI() {
         super("Main");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,31 +19,40 @@ public class RaceUI extends JFrame {
         HorseLanes horseLanes = new HorseLanes(3);
         this.horseLanes = horseLanes;
 
-        JPanel horseInfo = new JPanel();
+        horseInfo = new JPanel();
         horseInfo.setPreferredSize(new Dimension((int) (this.getWidth()*0.2), (int) (this.getHeight()*0.7)));
         horseInfo.setBackground(Color.BLUE);
 
-        Settings customization =  new Settings();
+        customization =  new Settings();
 
 
 
         this.addComponentListener(new java.awt.event.ComponentAdapter() {
             public void componentResized(java.awt.event.ComponentEvent evt) {
-                horseLanes.setPreferredSize(new Dimension((int) (RaceUI.super.getWidth()*0.8), (int) (RaceUI.super.getHeight()*0.7)));
-                horseInfo.setPreferredSize(new Dimension((int) (RaceUI.super.getWidth()*0.2), (int) (RaceUI.super.getHeight()*0.7)));
-                customization.setPreferredSize(new Dimension((int) RaceUI.super.getWidth(), (int) (RaceUI.super.getHeight()*0.3)));
-                horseInfo.revalidate();
-                horseLanes.revalidate();
-                customization.revalidate();
-                horseInfo.repaint();
-                horseLanes.repaint();
-                customization.repaint();
+                handleResize();
             }
         });
 
         this.add(horseInfo, BorderLayout.EAST);
         this.add(horseLanes, BorderLayout.WEST);
         this.add(customization, BorderLayout.SOUTH);
+
+
+        handleResize();
+
+    }
+
+
+    public void handleResize() {
+        horseLanes.setPreferredSize(new Dimension((int) (RaceUI.super.getWidth()*0.8), (int) (RaceUI.super.getHeight()*0.7)));
+        horseInfo.setPreferredSize(new Dimension((int) (RaceUI.super.getWidth()*0.2), (int) (RaceUI.super.getHeight()*0.7)));
+        customization.setPreferredSize(new Dimension((int) RaceUI.super.getWidth(), (int) (RaceUI.super.getHeight()*0.3)));
+        horseInfo.revalidate();
+        horseLanes.revalidate();
+        customization.revalidate();
+        horseInfo.repaint();
+        horseLanes.repaint();
+        customization.repaint();
     }
 
     public HorseLanes getHorseLanes() {
