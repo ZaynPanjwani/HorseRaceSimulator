079be3015bd2e7f3e9228c74ab9d16f00fda2b45
zaynpanjwani <zaynpanjwani@gmail.com>
2024-04-24 15:23:32 +0100

    Restructured the Race UI to be in its own class and created a public static instance of race ui in the main class which can be accessed from anywhere.

---
+++ Part 2/RaceUI.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RaceUI extends JFrame {
+    public RaceUI() {
+        super("Main");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(400, 400);
+        this.setLayout(new BorderLayout());
+
+        JPanel horseLanes = new JPanel();
+        horseLanes.setLayout(new BoxLayout(horseLanes, BoxLayout.Y_AXIS));
+        horseLanes.setBackground(Color.RED);
+        horseLanes.setPreferredSize(new Dimension((int) (this.getWidth()*0.8), (int) (this.getHeight()*0.7)));
+
+        JPanel horseInfo = new JPanel();
+        horseInfo.setPreferredSize(new Dimension((int) (this.getWidth()*0.2), (int) (this.getHeight()*0.7)));
+        horseInfo.setBackground(Color.BLUE);
+
+        JPanel customization =  new JPanel();
+        customization.setBackground(Color.GREEN);
+        customization.setPreferredSize(new Dimension((int) this.getWidth(), (int) (this.getHeight()*0.3)));
+
+
+        this.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentResized(java.awt.event.ComponentEvent evt) {
+                horseLanes.setPreferredSize(new Dimension((int) (RaceUI.super.getWidth()*0.8), (int) (RaceUI.super.getHeight()*0.7)));
+                horseInfo.setPreferredSize(new Dimension((int) (RaceUI.super.getWidth()*0.2), (int) (RaceUI.super.getHeight()*0.7)));
+                customization.setPreferredSize(new Dimension((int) RaceUI.super.getWidth(), (int) (RaceUI.super.getHeight()*0.3)));
+                horseInfo.revalidate();
+                horseLanes.revalidate();
+                customization.revalidate();
+                horseInfo.repaint();
+                horseLanes.repaint();
+                customization.repaint();
+            }
+        });
+
+        this.add(horseInfo, BorderLayout.EAST);
+        this.add(horseLanes, BorderLayout.WEST);
+        this.add(customization, BorderLayout.SOUTH);
+
+
+
+        this.setVisible(true);
+    }
+}
